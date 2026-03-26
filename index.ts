465ba920ab23c6d47f2ca5e4bde8265bf79e405e
Rahulkirar098 <kirarrahul1996@gmail.com>
2026-03-26 19:57:30 +0530

    Add FB INIT

--- index.ts
+++ index.ts
@@ -34,7 +34,13 @@ export type RiderPaymentStatus =
    INIT
 ========================================================= */
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
 
 const app = express();
 app.use(cors());
